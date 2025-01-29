34e539d62934e16899f3c1035854822efd1f8c09
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-01-29 14:24:55 -0400

    Project Started

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject circle;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
